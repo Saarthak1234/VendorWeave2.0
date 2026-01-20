659d59ae531aef7843f617f43e410535cf3a3eae
Saarthak1234 <saarthakagarwal0408@gmail.com>
2026-01-20 12:52:00 +0530

    Update vendor controllers to check firms exist or not

--- Server/contollers/VendorControllers/vendor.controller.js
+++ Server/contollers/VendorControllers/vendor.controller.js
@@ -1,3 +1,4 @@
+import Firm from "../../models/firmModel/firmModel.js";
 import Vendor from "../../models/vendorModel/vendorModel.js";
 import dotenv from 'dotenv'
 
@@ -11,6 +12,12 @@ const createVendor = async (req, res) => {
         const parentFirmId = req.params.firmId
 
         //Optional : Check if this firm exists or not?
+
+        const checkFirmExists = await Firm.findById(parentFirmId);
+        if (!checkFirmExists) {
+            console.log("Please provide a valid firm")
+            return res.status(400).json({ message: "Error creating vendor: Invalid firm" });
+        }
 
         //Add validations for creating vendor
         const checkVendorExists = await Vendor.findOne({ vendorName: name, firmId: parentFirmId });
@@ -63,38 +70,38 @@ const updateVendor = async (req, res) => {
             return res.status(400).json({ message: "Error updating vendor : Vendor does not exist" });
         }
 
-        if(checkVendorExists.vendorName == updatedName){
+        if (checkVendorExists.vendorName == updatedName) {
             console.log("Cannot enter the same name for the vendor")
-            return res.status(400).json({message:"Error updating vendor : Cannot enter the same name"})
+            return res.status(400).json({ message: "Error updating vendor : Cannot enter the same name" })
         }
 
-        if(updatedHealthScore < 0 || updatedHealthScore > 100){
+        if (updatedHealthScore < 0 || updatedHealthScore > 100) {
             console.log("Please enter a valid health score")
-            return res.status(400).json({message: "Error updating vendor : Please enter a valid healthscore"})
+            return res.status(400).json({ message: "Error updating vendor : Please enter a valid healthscore" })
         }
 
-        if(updatedPoints < 0){
+        if (updatedPoints < 0) {
             console.log("Please enter valid points")
-            return res.status(400).json({message : "Error updating vendor : Please enter valid points"})
+            return res.status(400).json({ message: "Error updating vendor : Please enter valid points" })
         }
 
         //Check for null values because updating all fields is not necessary
 
-        if(updatedName){
+        if (updatedName) {
             checkVendorExists.vendorName = updatedName
         }
-        if(updatedHealthScore){
+        if (updatedHealthScore) {
             checkVendorExists.healthScore = updatedHealthScore
         }
-        if(updatedPoints){
+        if (updatedPoints) {
             checkVendorExists.points = updatedPoints
         }
 
         const updateVendorDetails = await checkVendorExists.save()
 
-        console.log("Vendor updated Successfully",updateVendorDetails)
+        console.log("Vendor updated Successfully", updateVendorDetails)
 
-        return res.status(200).json({message:"Vendor details updated successfully"})
+        return res.status(200).json({ message: "Vendor details updated successfully" })
 
 
     } catch (error) {
@@ -104,18 +111,18 @@ const updateVendor = async (req, res) => {
 
 }
 
-const deleteVendor = async function(req,res){
+const deleteVendor = async function (req, res) {
     try {
-        const {name, vendor_id} = req.body
+        const { name, vendor_id } = req.body
         const FirmId = req.params
 
-        console.log(name,vendor_id,FirmId.firmId)
+        console.log(name, vendor_id, FirmId.firmId)
 
-        const checkVendorExists = await Vendor.findOne({vendorName : name , firmId : FirmId.firmId , _id : vendor_id})
+        const checkVendorExists = await Vendor.findOne({ vendorName: name, firmId: FirmId.firmId, _id: vendor_id })
 
-        if(!checkVendorExists){
+        if (!checkVendorExists) {
             console.log("Vendor not found")
-            return res.status(400).json({message : "Vendor not found"})
+            return res.status(400).json({ message: "Vendor not found" })
         }
 
         const deleteVendor = await checkVendorExists.deleteOne();
@@ -123,43 +130,76 @@ const deleteVendor = async function(req,res){
         console.log(deleteVendor)
 
         console.log("Vendor successfully deleted")
-        return res.status(200).json({message : "Vendor successfully deleted"})
+        return res.status(200).json({ message: "Vendor successfully deleted" })
 
     } catch (error) {
         console.log("Error in deleting vendor");
-        return res.status(500).json({message:"Internal Server error : Error while deleting firm"})
+        return res.status(500).json({ message: "Internal Server error : Error while deleting firm" })
     }
 }
 
-const getVendorByFirmId = async(req,res) => {
+const getVendorByFirmId = async (req, res) => {
     try {
-        const {name} = req.body
+        const { name } = req.body
         const firmId = req.params.firmId
 
-        console.log(name, firmId)
+        const checkVendorExists = await Vendor.findOne({ vendorName: name, firmId: firmId })
 
-        const checkVendorExists = await Vendor.findOne({vendorName: name, firmId: firmId})
-
-        if(!checkVendorExists){
+        if (!checkVendorExists) {
             console.log("Vendor with these details for this firm does not exist")
-            return res.status(400).json({message: "Vendor not found"})
+            return res.status(400).json({ message: "Vendor not found" })
         }
 
         const vendorObj = {
-            name : checkVendorExists.vendorName,
-            healthScore : checkVendorExists. healthScore,
+            name: checkVendorExists.vendorName,
+            healthScore: checkVendorExists.healthScore,
             points: checkVendorExists.points
         }
 
         console.log(vendorObj);
         return res.status(200).json({
-            message:"Vendor found and returned",vendorObj})
-        
+            message: "Vendor found and returned", vendorObj
+        })
+
 
     } catch (error) {
         console.log("Error in get Vendor By ID")
-        return res.status(500).json({message:"Internal Server Error : Error while fetching details of this vendor for this firm"})
+        return res.status(500).json({ message: "Internal Server Error : Error while fetching details of this vendor for this firm" })
     }
 }
 
-export {createVendor, updateVendor, deleteVendor, getVendorByFirmId}
+const getVendorsByFirmId = async (req, res) => {
+    try {
+        const recievedFirmId = req.params
+
+        // Add a check if that firm exists or not
+
+        // const checkFirmExists = await Firm.findById(recievedFirmId.firmId)
+        // console.log(recievedFirmId, recievedFirmId.firmId)
+
+        // console.log(checkFirmExists)
+
+        // if(!checkFirmExists){
+        //     console.log("Please enter a valid firm")
+        //     return res.status(400).json({message : "Invalid firm"})
+        // }
+
+        const getAllVendors = await Vendor.find(recievedFirmId)
+        console.log(getAllVendors)
+
+        if(getAllVendors === null || getAllVendors.length === 0){
+            console.log("No vendor exists")
+            return res.status(400).json("No vendor for this firm exists")
+        }
+
+        console.log("Vendors returned successfully")
+
+        return res.status(200).json({ message: "Vendors successfully found and returned" })
+
+    } catch (error) {
+        console.log("Error in getting all the vendors of a particular firm",error)
+        return res.status(500).json({ message: "Internal Server error : Error while getting vendors of that particular firm" })
+    }
+}
+
+export { createVendor, updateVendor, deleteVendor, getVendorByFirmId, getVendorsByFirmId}
